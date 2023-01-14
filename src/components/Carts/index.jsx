785ec66f10a63bd9c7d305c0390339905c9e3781
Easin Webpro <easinislam299@gmail.com>
2023-01-14 20:09:25 +0600

    ✨ [components/carts] - add empty_cart ui in cart component

--- src/components/Carts/index.jsx
+++ src/components/Carts/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Cart_Desktop_V } from './Cart_Desktop_V';
+import { EmptyUi } from 'components/UI/EmptyUi';
+import {animationData} from 'static/raw';
 
 export const Cart_Holder = ({ products }) => {
   return (
     <div>
-      <Cart_Desktop_V products={products} />
+      {
+        Object.keys(products).length === 0 ?
+          <EmptyUi title="Cart Empty - No Product" ani_src={animationData?.empty_cart} />
+          :
+          <Cart_Desktop_V products={products} />
+      }
+
     </div>
   )
 };
