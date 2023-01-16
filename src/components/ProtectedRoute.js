ea2c877cc94c9b2cc7f018848a3426da4f85c34e
Easin Webpro <easinislam299@gmail.com>
2023-01-16 19:11:46 +0600

    ♻️ - [components/loader] Refactor all components-loader move ui-loader ...

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
-import { PageLoader } from './Loader';
+import { PageLoader } from './UI/Loader';
 
 export const IsAuthenticated = ({ children }) => {
 
