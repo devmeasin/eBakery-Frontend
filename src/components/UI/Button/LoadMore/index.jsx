2811fcbebcb13a54fb48d71436719caa728ad12e
Easin Webpro <easinislam299@gmail.com>
2023-01-23 20:48:07 +0600

    ⚡️ [components/ui] improved loadmore btn prev ui & func  & simplify

--- src/components/UI/Button/LoadMore/index.jsx
+++ src/components/UI/Button/LoadMore/index.jsx
@@ -1,26 +1,16 @@
 import { Button } from "@mantine/core";
+import { IconAssembly } from "@tabler/icons";
 
-export const LoadMore = ({ meta, setPage , loader }) => {
-
-    if(!meta) return;
-    const { page, pageCount } = meta?.pagination;
-
-    const handlePaginate = () => {
-        setPage(page + 1);
-    };
+export const LoadMore = ({ handlePaginate, loader = false }) => {
 
     return (
-        <div style={{ display: 'flex', alignItem : 'center' , justifyContent:'center', padding : '30px 0px 10px'}}>
-            {
-                page >= pageCount ? '' :
-                    <Button radius='lg' variant="gradient" gradient={{ from: 'orange', to: 'red' }}
-                        onClick={() => handlePaginate()}
-                        loading={loader}
-                    >
-                        Load More
-                    </Button>
-            }
-
+        <div style={{ display: 'flex', alignItem: 'center', justifyContent: 'center', padding: '30px 0px 10px' }}>
+            <Button radius="xl" variant="gradient" gradient={{ from: 'lime', to: 'teal', deg: 105 }}
+                onClick={() => handlePaginate()}
+                loading={loader}
+            >
+                Load More {loader === false && <IconAssembly size='20px' style={{marginLeft:'5px'}}/>}
+            </Button>
         </div>
     );
 };
