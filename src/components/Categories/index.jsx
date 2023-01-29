e78eca19c0b79888588024d2ffce704b13d65a08
Easin Webpro <easinislam299@gmail.com>
2023-01-29 23:59:47 +0600

    🚧[src/page] inprogress category page cotegory gird design

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,4 +1,5 @@
 import { Carousel } from '@mantine/carousel';
+import { Grid } from '@mantine/core';
 import { Category } from 'components/Categories/Category';
 import { CategoryLoader } from 'components/UI/Loader/CategoryLoader';
 import { useGetCategoryDataQuery } from 'store/services/categoriesApi';
@@ -10,24 +11,47 @@ export const Categories = ({ categoryType = 'grid' }) => {
 	const categoriesData = categories_dtos(data);
 
 	return (
-		categoriesData?.length > 0 ?
-			<Carousel
-				slideSize='100px'
-				height={120}
-				align='start'
-				slideGap='sm'
-				controlSize={categoriesData?.length}
-				loop
-				dragFree
-			>
-				{
-					categoriesData?.map((category) => (
-						<Carousel.Slide key={category.name}>
-							<Category category={category} />
-						</Carousel.Slide>
-					))
-				}
-			</Carousel> : <CategoryLoader />
+
+		categoryType === 'grid' ?
+
+			(
+				categoriesData?.length > 0 ?
+					<Grid >
+						{
+							categoriesData?.map((category) => (
+								<Grid.Col  md={2} lg={1} key={category.name}>
+									<Category category={category} />
+								</Grid.Col>
+
+							))
+						}
+					</Grid>
+
+					: <CategoryLoader />
+			)
+
+			:
+
+			(
+				categoriesData?.length > 0 ?
+					<Carousel
+						slideSize='100px'
+						height={120}
+						align='start'
+						slideGap='sm'
+						controlSize={categoriesData?.length}
+						loop
+						dragFree
+					>
+						{
+							categoriesData?.map((category) => (
+								<Carousel.Slide key={category.name}>
+									<Category category={category} />
+								</Carousel.Slide>
+							))
+						}
+					</Carousel> : <CategoryLoader />
+			)
 
 	);
 };
